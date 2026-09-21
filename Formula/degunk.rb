4547b903ae6b25d2ceaa5006e66c4b48966d5420
Abdullah Hassan <163249846+AbdullahHassan192@users.noreply.github.com>
2026-09-21 23:54:45 +0500

    bump: update formula to v0.1.2

--- Formula/degunk.rb
+++ Formula/degunk.rb
@@ -1,25 +1,25 @@
 class Degunk < Formula
   desc "Interactive dependency and build artifact cleaner"
   homepage "https://github.com/AbdullahHassan192/degunk"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/AbdullahHassan192/degunk/releases/download/v0.1.1/degunk-darwin-arm64.tar.gz"
-      sha256 "2d6c8e574f2cb6dab471be7a2a2ee5cfbc1b26a976e14bfd8ed9a266ded41475"
+      url "https://github.com/AbdullahHassan192/degunk/releases/download/v0.1.2/degunk-darwin-arm64.tar.gz"
+      sha256 "5464c65b713c763483129f9f36a15f234c517ac1bed330c8aeae6381c9dffa71"
     else
-      url "https://github.com/AbdullahHassan192/degunk/releases/download/v0.1.1/degunk-darwin-x86_64.tar.gz"
-      sha256 "8b0d8138890e26576b57aaf5f36d52029083ebb3e63e58a862504f19ba3a3cd7"
+      url "https://github.com/AbdullahHassan192/degunk/releases/download/v0.1.2/degunk-darwin-x86_64.tar.gz"
+      sha256 "371f4eb05e60401944ed1819a5fe3beb31c607092fb6ea9089c2cb00580da016"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/AbdullahHassan192/degunk/releases/download/v0.1.1/degunk-linux-arm64.tar.gz"
-      sha256 "9d691945ceb6a0ec168aecb0a8b209e8327d0f15447afde7d628acab734c714c"
+      url "https://github.com/AbdullahHassan192/degunk/releases/download/v0.1.2/degunk-linux-arm64.tar.gz"
+      sha256 "5c394ee274055cd1dcab2b412c89c57fa7679a37250c2bec6d84aff6c9df8fd0"
     else
-      url "https://github.com/AbdullahHassan192/degunk/releases/download/v0.1.1/degunk-linux-x86_64.tar.gz"
-      sha256 "69ae0f10bbaf55c51942f7f9a2a22f4dfa00f381b7966b5aee482897b1901cdc"
+      url "https://github.com/AbdullahHassan192/degunk/releases/download/v0.1.2/degunk-linux-x86_64.tar.gz"
+      sha256 "94829ce493c790a34fe9df28d48c81f1ddfc54f559e1d543f7bf6ca5244d2ff8"
     end
   end
 
